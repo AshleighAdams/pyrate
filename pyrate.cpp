5a324954ec894f764f6b25cf3f41d081c1857ab4
Ole <itstheimp@gmail.com>
2013-05-22 19:36:38 +0200

    There is a license?

--- pyrate.cpp
+++ pyrate.cpp
@@ -1,3 +1,20 @@
+/*
+    This file is part of pyrate.
+
+    pyrate is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    pyrate is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with pyrate.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 #include <lua.hpp>
 #include <thread>
 #include <mutex>
